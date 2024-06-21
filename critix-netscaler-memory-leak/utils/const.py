04643d7627b04070cd0af19e9ab7cce625718305
Karthikeyan C <114332111+cyber-karthi@users.noreply.github.com>
2024-06-21 14:17:37 +0530

    Update const.py

--- critix-netscaler-memory-leak/utils/const.py
+++ critix-netscaler-memory-leak/utils/const.py
@@ -12,7 +12,7 @@
 
 
 class Data:
-    blog = 'https://blogs.cappriciosec.com/blog/137/critix-netscaler-memory-leak'
+    blog = 'https://blogs.cappriciosec.com/blog/167/critix-netscaler-memory-leak'
     api = 'https://api.cappriciosec.com/Telegram/cappriciosecbot.php'
     config_path = '~/.config/cappriciosec-tools/cappriciosec.yaml'
     payloadurl = 'https://raw.githubusercontent.com/Cappricio-Securities/PayloadAllTheThings/main/critix-netscaler-memory-leak.txt'
